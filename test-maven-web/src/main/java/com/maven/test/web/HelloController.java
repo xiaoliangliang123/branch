330064432824d58ea4b5356db1d810af4c2ea601
wangliang <123456>
2018-06-10 11:24:38 +0800

    第一次提交，整个项目

---
+++ test-maven-web/src/main/java/com/maven/test/web/HelloController.java
@@ -0,0 +1,22 @@
+package com.maven.test.web;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+public class HelloController {
+
+	@RequestMapping("/")
+	@ResponseBody
+    public String hello(){
+        return "Greetings from Spring Boot!";
+    }
+	
+	
+	@RequestMapping("/test")
+	@ResponseBody
+    public String test(){
+        return "Greetings from Spring Boot!";
+    }
+}
